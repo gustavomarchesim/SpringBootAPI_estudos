994919e46a9aabf18ca9777ef729cf82aa975f09
gustavomarchesim <gustavomarchesim@outlook.com>
2024-09-09 15:03:48 -0300

    feature: Listagem de médicos

--- src/main/java/dev/gdam/voll/api/controller/MedicoController.java
+++ src/main/java/dev/gdam/voll/api/controller/MedicoController.java
@@ -1,12 +1,16 @@
 package dev.gdam.voll.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.gdam.voll.api.dto.ListagemMedicoDTO;
 import dev.gdam.voll.api.dto.MedicoDTO;
 import dev.gdam.voll.api.entity.medico.Medico;
 import dev.gdam.voll.api.repository.MedicoRepository;
@@ -23,5 +27,10 @@ public class MedicoController {
   @Transactional
   public void cadastro(@RequestBody @Valid MedicoDTO dados) {
     repository.save(new Medico(dados));
+  }
+
+  @GetMapping
+  public Page<ListagemMedicoDTO> listar(Pageable paginacao) {
+    return repository.findAll(paginacao).map(ListagemMedicoDTO::new);
   }
 }
